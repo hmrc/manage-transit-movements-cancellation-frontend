996b94f12615bd902ac901d87e00c0716b104c67
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2024-02-16 15:42:28 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-30"                    % mongoVersion,
     "uk.gov.hmrc"          %% "play-conditional-form-mapping-play-30" % "2.0.0",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"            % "8.0.0",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"            % "8.5.0",
     "com.lucidchart"       %% "xtract"                                % "2.3.0",
     "uk.gov.hmrc"          %% "crypto-json-play-30"                   % "7.6.0"
   )
