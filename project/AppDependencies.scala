626e5a570c0f1487e9626ea629cda9e9c8b5171c
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-07-04 07:03:07 +0100

    CTCP-5506: update dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-30"                    % mongoVersion,
     "uk.gov.hmrc"          %% "play-conditional-form-mapping-play-30" % "2.0.0",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"            % "10.0.0",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"            % "10.3.0",
     "com.lucidchart"       %% "xtract"                                % "2.3.0",
     "uk.gov.hmrc"          %% "crypto-json-play-30"                   % "8.0.0",
     "javax.xml.bind"        % "jaxb-api"                              % "2.3.1"
