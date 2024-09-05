711a74f624aa258f8d8c88cb3d0f45d02bd8c29a
Kevin Coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2024-09-05 11:58:46 +0100

    CTCP-5781: Removed xtract library as not required for Phase 5 repo's

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,6 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "play-conditional-form-mapping-play-30" % "3.1.0",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"            % "10.3.0",
-    //"com.lucidchart"       %% "xtract"                                % "2.3.0",
     "uk.gov.hmrc"          %% "crypto-json-play-30"                   % "8.0.0",
     "javax.xml.bind"        % "jaxb-api"                              % "2.3.1",
     "org.typelevel"        %% "cats-core"                             % "2.12.0"
