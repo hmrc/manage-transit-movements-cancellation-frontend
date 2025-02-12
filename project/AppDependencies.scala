982a4ca0995e8be8f416e304fec46385b470c082
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2025-02-12 12:10:26 +0000

    CTCP-6206 Upgrade play-frontend-hmrc to 11.11.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object AppDependencies {
 
   private val mongoVersion = "2.3.0"
-  private val bootstrapVersion = "9.6.0"
+  private val bootstrapVersion = "9.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-30"                    % mongoVersion,
     "uk.gov.hmrc"          %% "play-conditional-form-mapping-play-30" % "3.2.0",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"            % "11.9.0",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"            % "11.11.0",
     "uk.gov.hmrc"          %% "crypto-json-play-30"                   % "8.1.0",
     "javax.xml.bind"        % "jaxb-api"                              % "2.3.1",
     "org.typelevel"        %% "cats-core"                             % "2.12.0"
