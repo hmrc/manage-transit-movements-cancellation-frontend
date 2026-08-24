1ffbbe9763ef1e15f7edee96cec89ad92c5178e6
prashanthmamidi <6618159+prashanthmamidi@users.noreply.github.com>
2026-08-24 18:26:49 +0100

    DL-20004 removed phase 6 feature flags and move any divergent logic into the Phase 6 version where found

--- it/test/connectors/DepartureMovementConnectorSpec.scala
+++ it/test/connectors/DepartureMovementConnectorSpec.scala
@@ -30,11 +30,8 @@ import scala.xml.{Node, NodeSeq}
 
 class DepartureMovementConnectorSpec extends ItSpecBase with WireMockServerHandler {
 
-  private lazy val phase5App: GuiceApplicationBuilder => GuiceApplicationBuilder =
-    _ => guiceApplicationBuilder().configure("feature-flags.phase-6-api-enabled" -> false)
-
   private lazy val phase6App: GuiceApplicationBuilder => GuiceApplicationBuilder =
-    _ => guiceApplicationBuilder().configure("feature-flags.phase-6-api-enabled" -> true)
+    _ => guiceApplicationBuilder()
 
   override def guiceApplicationBuilder(): GuiceApplicationBuilder =
     super
@@ -107,23 +104,6 @@ class DepartureMovementConnectorSpec extends ItSpecBase with WireMockServerHandl
         )
       )
 
-      "when phase 5" - {
-        "must return Messages" in {
-
-          running(phase5App) {
-            app =>
-              val connector: DepartureMovementConnector = app.injector.instanceOf[DepartureMovementConnector]
-
-              server.stubFor(
-                get(urlEqualTo(s"/movements/departures/$departureId/messages"))
-                  .withHeader("Accept", equalTo("application/vnd.hmrc.2.1+json"))
-                  .willReturn(okJson(responseJson.toString()))
-              )
-
-              connector.getMessageMetaData(departureId).futureValue mustEqual expectedResult
-          }
-        }
-      }
 
       "when phase 6" - {
         "must return Messages" in {
@@ -149,119 +129,6 @@ class DepartureMovementConnectorSpec extends ItSpecBase with WireMockServerHandl
       val messageId = "messageId"
       val url       = s"/movements/departures/$departureId/messages/$messageId/body"
 
-      "when phase 5" - {
-        "must return message" - {
-          "when IE015" in {
-            import models.IE015.*
-
-            running(phase5App) {
-              app =>
-                val connector: DepartureMovementConnector = app.injector.instanceOf[DepartureMovementConnector]
-
-                val xml: Node =
-                  <ncts:CC015C xmlns:ncts="http://ncts.dgtaxud.ec">
-                    <messageSender>message sender</messageSender>
-                    <messageRecipient>NTA.GB</messageRecipient>
-                    <preparationDateAndTime>2022-01-22T07:43:36</preparationDateAndTime>
-                    <messageIdentification>messageId</messageIdentification>
-                    <messageType>CC015C</messageType>
-                    <TransitOperation>
-                      <LRN>HnVr</LRN>
-                      <declarationType>Pbg</declarationType>
-                      <additionalDeclarationType>A</additionalDeclarationType>
-                      <security>1</security>
-                      <reducedDatasetIndicator>1</reducedDatasetIndicator>
-                      <bindingItinerary>0</bindingItinerary>
-                    </TransitOperation>
-                    <CustomsOfficeOfDeparture>
-                      <referenceNumber>GB000060</referenceNumber>
-                    </CustomsOfficeOfDeparture>
-                    <CustomsOfficeOfDestinationDeclared>
-                      <referenceNumber>XI000142</referenceNumber>
-                    </CustomsOfficeOfDestinationDeclared>
-                    <HolderOfTheTransitProcedure>
-                      <identificationNumber>idNumber</identificationNumber>
-                    </HolderOfTheTransitProcedure>
-                    <Consignment>
-                      <grossMass>6430669292.48125</grossMass>
-                    </Consignment>
-                  </ncts:CC015C>
-
-                val expectedResult = IE015(
-                  transitOperation = TransitOperation(
-                    lrn = "HnVr"
-                  ),
-                  customsOfficeOfDeparture = CustomsOfficeOfDeparture(
-                    referenceNumber = "GB000060"
-                  ),
-                  holderOfTheTransitProcedure = HolderOfTheTransitProcedure(
-                    identificationNumber = Some("idNumber"),
-                    tirHolderIdentificationNumber = None,
-                    name = None,
-                    address = None,
-                    contactPerson = None
-                  )
-                )
-
-                server.stubFor(
-                  get(urlEqualTo(url))
-                    .withHeader("Accept", equalTo("application/vnd.hmrc.2.1+xml"))
-                    .willReturn(ok(xml.toString()))
-                )
-
-                val result = connector.getMessage[IE015](departureId, messageId).futureValue
-
-                result mustEqual expectedResult
-            }
-
-          }
-
-          "when IE028" in {
-            import models.IE028.*
-
-            running(phase5App) {
-              app =>
-                val connector: DepartureMovementConnector = app.injector.instanceOf[DepartureMovementConnector]
-
-                val xml: Node =
-                  <ncts:CC028C xmlns:ncts="http://ncts.dgtaxud.ec">
-                    <messageSender>message sender</messageSender>
-                    <messageRecipient>NTA.GB</messageRecipient>
-                    <preparationDateAndTime>2022-12-25T07:36:28</preparationDateAndTime>
-                    <messageIdentification>messageId</messageIdentification>
-                    <messageType>CC028C</messageType>
-                    <TransitOperation>
-                      <LRN>LRN123</LRN>
-                      <MRN>3817-MRNAllocated2</MRN>
-                      <declarationAcceptanceDate>2022-12-25</declarationAcceptanceDate>
-                    </TransitOperation>
-                    <CustomsOfficeOfDeparture>
-                      <referenceNumber>GB000060</referenceNumber>
-                    </CustomsOfficeOfDeparture>
-                    <HolderOfTheTransitProcedure>
-                      <identificationNumber>Fzsisks</identificationNumber>
-                    </HolderOfTheTransitProcedure>
-                  </ncts:CC028C>
-
-                val expectedResult = IE028(
-                  transitOperation = TransitOperation(
-                    mrn = "3817-MRNAllocated2"
-                  )
-                )
-
-                server.stubFor(
-                  get(urlEqualTo(url))
-                    .withHeader("Accept", equalTo("application/vnd.hmrc.2.1+xml"))
-                    .willReturn(ok(xml.toString()))
-                )
-
-                val result = connector.getMessage[IE028](departureId, messageId).futureValue
-
-                result mustEqual expectedResult
-            }
-          }
-        }
-      }
 
       "when phase 6" - {
         "must return message" - {
@@ -386,25 +253,6 @@ class DepartureMovementConnectorSpec extends ItSpecBase with WireMockServerHandl
           <messageSender>token</messageSender>
         </ncts:CC014C>
 
-      "when phase 5" - {
-        "must return OK for successful response" in {
-          running(phase5App) {
-            app =>
-              val connector: DepartureMovementConnector = app.injector.instanceOf[DepartureMovementConnector]
-              server.stubFor(
-                post(urlEqualTo(url))
-                  .withRequestBody(equalTo(body.toString()))
-                  .withHeader("Accept", equalTo("application/vnd.hmrc.2.1+json"))
-                  .withHeader("Content-Type", equalTo("application/xml"))
-                  .willReturn(ok())
-              )
-
-              val result = connector.submit(body, DepartureId(departureId)).futureValue
-
-              result.status mustEqual OK
-          }
-        }
-      }
 
       "when phase 6" - {
         "must return OK for successful response" in {
